5c53a919399da948d38a47beebcdab39ec0538ba
Paul Knight <paul@diplograph.net>
2017-07-29 21:14:40 -0700

    Add a class for looking up localized strings.

--- www/flame.js
+++ www/flame.js
@@ -1,27 +1,53 @@
 var localizedStrings = {
-"website_title" : ["Radiance @ Smukfest", "Radiance @ Smukfest"],
-
-"cookie_short" : ["flame.vision uses cookies to help count votes. We don't use cookies for marketing or any other tracking reasons. Is that cool with you?", ""],
-
-"cookie_expanded" : ["We assign each visitor a random number and store it in a cookie to keep track of your votes. The random number will expire on September 1, 2017. We may use these identifiers for analytics (such as answering \"how many people voted?\") or to detect cheaters (please don't do this). But we won't use these identifiers for any kind of commercial or marketing purposes. Oh, we also use local storage to record if you accept this cookie. If you don't accept this, please don't use flame.vision", ""],
-
-"cookie_expand_button" : ["More Info", ""],
-"cookie_accept_button" : ["Okay", ""],
-
-"loading_placeholder" : ["THIS IS IT", ""],
-"loading_placeholder_2" : ["STAND BY", ""],
-
-"not_running_title" : ["🔥 Radiance @ Smukfest 🇩🇰", ""],
-"not_running_dates" : ["August 9 – 13", ""],
-"not_running_times" : ["21:00 – late", ""],
-"not_running_weather_notice" : ["(weather permitting)", ""],
-
-"ardent_alt_text" : ["Ardent Heavy Industries", ""],
-
-"player_name_placeholder" : ["@player1", ""],
-"player_ranking_title" : ["How Awesome?", ""]
-}
-
+  "cookie_accept_button": [
+    "Okay",
+    ""
+  ],
+  "cookie_expand_button": [
+    "More Info",
+    ""
+  ],
+  "cookie_expanded": [
+    "We assign each visitor a random number and store it in a cookie to keep track of your votes. The random number will expire on September 1, 2017. We may use these identifiers for analytics (such as answering \"how many people voted?\") or to detect cheaters (please don't do this). But we won't use these identifiers for any kind of commercial or marketing purposes. Oh, we also use local storage to record if you accept this cookie. If you don't accept this, please don't use flame.vision",
+    ""
+  ],
+  "cookie_short": [
+    "flame.vision uses cookies to help count votes. We don't use cookies for marketing or any other tracking reasons. Is that cool with you?",
+    ""
+  ],
+  "loading_placeholder": [
+    "STAND BY",
+    ""
+  ],
+  "not_running_dates": [
+    "August 9 – 13",
+    ""
+  ],
+  "not_running_times": [
+    "21:00 – late",
+    ""
+  ],
+  "not_running_title": [
+    "🔥 Radiance @ Smukfest 🇩🇰",
+    ""
+  ],
+  "not_running_weather_notice": [
+    "(weather permitting)",
+    ""
+  ],
+  "player_name_placeholder": [
+    "@player1",
+    ""
+  ],
+  "player_ranking_title": [
+    "How Awesome?",
+    ""
+  ],
+  "website_title": [
+    "Radiance @ Smukfest",
+    "Radiance @ Smukfest"
+  ]
+};
 
 var API = function() {};
 
@@ -42,6 +68,53 @@ API.prototype = {
 }
 
 var api = new API();
+
+var LocalizedStringManager = function() {
+    this.languageIndex = undefined; // Index into the localizedStrings object.
+
+    var savedLangaugePreference = window.localStorage.getItem("preferredLanguage");
+    if (savedLangaugePreference === "0") {
+        this.languageIndex = 0;
+    } else if (savedLangaugePreference === "1") {
+        this.languageIndex = 1;
+    }
+
+    if (this.languageIndex === undefined) {
+        var navigatorLanguage = navigator.languageIndex;
+        if (navigatorLanguage && navigatorLanguage.indexOf("nl") !== -1) { // I have no idea if this is cool.
+            this.languageIndex = 1;
+        } else {
+            this.languageIndex = 0;
+        }
+    }
+}
+
+LocalizedStringManager.prototype = {
+    localizedStringForKey: function(key) {
+        var stringArray = localizedStrings[key];
+        if (!stringArray) {
+            return key;
+        }
+
+        if (stringArray.length < this.languageIndex) {
+            return key;
+        }
+
+        var localizedString = stringArray[this.languageIndex];
+        if (localizedString.length == 0) {
+            return key;
+        }
+
+        return localizedString;
+    },
+
+    setLanguage: function(languageIndex) {
+        this.languageIndex = languageIndex;
+        window.localStorage.setItem("preferredLanguage", languageIndex.toString());
+    },
+};
+
+localizedStringManager = new LocalizedStringManager();
 
 var Unicorn = function() {};
 
